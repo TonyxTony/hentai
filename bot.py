ac19c0e3dd475dc1950e2d6fa0296e96976807f2
TonyxTony <tonyxkiller43@gmail.com>
2025-02-01 09:17:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 from pyrogram import Client, filters
 import os
-import re
 import time
 from threading import Thread
 from flask import Flask
@@ -10,33 +9,26 @@ from pymongo import MongoClient
 import re
 from PIL import Image
 import imagehash
-import signal
 
 HANDLER = "."
 API_ID = "25321403"
 API_HASH = "0024ae3c978ba534b1a9bffa29e9cc9b"
-SESSION = "BQFo9VAALAHKuEpUHoCealAw8UnYRDLqDtWWGapgMKyDdDNqgra2Gnd2EnVwpwP4PvujFjRM1Lltr8qh1DeTheqRukQF_GPApLhtS2eldLOBWrNYogDqIGr6ifgNnMI1oQAzsMkne0-wkGgrobJyMrKKV3oodj3ast0XVmvtyzh1cutBwm9Ob-BCjS22hK3E5R9A8fL0jKczAM0YgY82TCp2SU9qvCSjPaKASSN2w8HVt8HvWBJWd7tKf0i6VSwIN-5USPrAejxgxpEIwVumBZKTu6wpP2AeWADFN_OCaLTf_hD7klLnBffR6obkodGkIX-ZczkrmX7TstXICIT7jdcxwEutwgAAAAGRx5e_AA"
+BOT_TOKEN = os.getenv('BOT_TOKEN', '7373345228:AAFzQdntjOGtHaEpsHbVYUsL8xQMacfd8G4')
 MONGO_URI = os.getenv('MONGO_URI', "mongodb+srv://Lakshay3434:Tony123@cluster0.agsna9b.mongodb.net/?retryWrites=true&w=majority")
 hexa_bot = 572621020
-
-ALLOWED_CHAT_IDS = [
-    -1002136935704, -1002244785813, -1002200182279, -1002232771623,
-    -1002241545267, -1002180680112, -1002152913531, -1002244523802,
-    -1002159180828, -1002186623520, -1002220460503
-]
 
 if not MONGO_URI:
     raise Exception("MONGO_URI environment variable is not set")
 
 mongo_client = MongoClient(MONGO_URI)
 db = mongo_client['grabber_db']
-hexaimg = db['hexa_img']
+hexa_status = db['hexa_hashes']
 
 app = Client(
     "word9",
     api_id=API_ID,
     api_hash=API_HASH,
-    session_string=SESSION
+    bot_token=BOT_TOKEN
 )
 
 server = Flask(__name__)
@@ -44,65 +36,24 @@ server = Flask(__name__)
 @server.route("/")
 def home():
     return "Bot is running"
-    
-@app.on_message(filters.chat(ALLOWED_CHAT_IDS) & filters.user(hexa_bot) & filters.regex(r"pokemon was"))
-async def capture_pokemon(client, message):
-    try:
-        cleaned_text = re.sub(r'(\*{1,2})(.*?)\1', r'\2', message.text.strip())
-        if "pokemon was" not in cleaned_text:
-            return
 
-        if message.reply_to_message and message.reply_to_message.photo:
-            replied_message = message.reply_to_message
-            file_id = replied_message.photo.file_id
-
-            existing_photo = hexaimg.find_one({"file_id": file_id})
-            if existing_photo:
-                await message.reply("photo already forwarded previously")
-                return
-
-            file_path = await client.download_media(replied_message.photo)
-            full_text = message.text.strip()
-
-            try:
-                # Send the photo with the caption directly together
-                sent_photo_message = await client.send_photo(
-                    '@Hexa_DB', 
-                    file_path, 
-                    caption=full_text  # Send the photo and caption together
-                )
-
-                sent_message_id = sent_photo_message.id
-                sent_chat_id = sent_photo_message.chat.id
-
-                hexaimg.insert_one({"file_id": file_id, "message_id": sent_message_id})
-
-                os.remove(file_path)
-
-            except Exception as send_error:
-                await message.reply(f"Error sending photo to @Hexa_DB: {send_error}")
-                return
-
-        else:
-            await message.reply("No photo found in the replied message.")
-
-    except Exception as e:
-        await message.reply(f"An error occurred: {str(e)}")
+ALLOWED_CHAT_IDS = [
+    -1002136935704, -1002244785813, -1002200182279, -1002232771623,
+    -1002241545267, -1002180680112, -1002152913531, -1002244523802,
+    -1002159180828, -1002186623520, -1002220460503
+]
 
 def hash_image(image_path):
-    try:
-        with Image.open(image_path) as img:
-            hash_value = imagehash.phash(img)
-            return str(hash_value)
-    except Exception as e:
-        return str(e)
+    with Image.open(image_path) as img:
+        hash_value = imagehash.phash(img)
+        return str(hash_value)
 
 @app.on_message(filters.user(hexa_bot) & filters.photo)
 async def handle_hexa_bot(client, message):
     try:
         file_path = await message.download()
         image_hash_value = hash_image(file_path)
-        existing_doc = hexacollection.find_one({"image_hash": image_hash_value})
+        existing_doc = hexa_status.find_one({"image_hash": image_hash_value})
 
         if existing_doc:
             pokemon_name = existing_doc.get("pokemon_name")
@@ -115,19 +66,53 @@ async def handle_hexa_bot(client, message):
     except Exception as e:
         print(f"Error handling hexa_bot: {e}")
 
-@app.on_message(filters.command("ding", HANDLER) & filters.me)
+@app.on_message(filters.chat(ALLOWED_CHAT_IDS) & filters.photo)
+async def capture_pokemon_data(client, message):
+    try:
+        if message.photo and "pokemon was" in message.text:
+            file_path = await client.download_media(message.photo)
+            image_hash_value = hash_image(file_path)
+            if 'Error' in image_hash_value:
+                await message.reply(f"Error generating hash: {image_hash_value}")
+                return
+
+            pokemon_name_match = re.search(r"The pokemon was (.*)", message.text)
+            if pokemon_name_match:
+                pokemon_name = pokemon_name_match.group(1).strip()
+                existing_doc = hexa_status.find_one({"image_hash": image_hash_value})
+                if existing_doc:
+                    await message.reply(f"The image `{image_hash_value}` already exists in DB!")
+                else:
+                    hexa_status.update_one(
+                        {"image_hash": image_hash_value},
+                        {"$set": {"image_hash": image_hash_value, "pokemon_name": pokemon_name}},
+                        upsert=True
+                    )
+                    await message.reply(f"Stored image `{image_hash_value}` with Pokémon name `{pokemon_name}` Added in DB!")
+    except Exception as e:
+        await message.reply(f"An error occurred while processing the request: {str(e)}")
+
+@app.on_message(filters.command("total_pokemon", HANDLER))
+async def total_pokemon(client: Client, message: Message):
+    try:
+        total_pokemon_count = hexa_status.count_documents({})
+        await message.reply(f"Pokémon in the database `{total_pokemon_count}` Otey!")
+    except Exception as e:
+        await message.reply(f"An error occurred while fetching the total Pokémon count: {str(e)}")
+
+@app.on_message(filters.command("ding", HANDLER))
 async def ping_pong(client: Client, message: Message):
     try:
         start_time = time.time()
         msg = await message.reply_text("Ping...")
-        await msg.edit("✮ᑭｴƝGing...✮")
+        await msg.reply("✮ᑭｴƝGing...✮")
         end_time = time.time()
         ping_time = round((end_time - start_time) * 1000, 3)
         
         uptime_seconds = time.time() - bot_start_time
         uptime_str = format_uptime(uptime_seconds)
         
-        await msg.edit(f"I Aᴍ Aʟɪᴠᴇ Mᴀꜱᴛᴇʀ\n⋙ 🔔 ᑭｴƝG: `{ping_time}` ms\n⋙ 🕒 Uptime: `{uptime_str}`")
+        await msg.reply(f"I Aᴍ Aʟɪᴠᴇ Mᴀꜱᴛᴇʀ\n⋙ 🔔 ᑭｴƝG: `{ping_time}` ms\n⋙ 🕒 Uptime: `{uptime_str}`")
         try:
             await message.delete()
         except Exception as e:
@@ -140,17 +125,6 @@ def format_uptime(seconds):
     minutes, seconds = divmod(remainder, 60)
     return f"{hours}h {minutes}m {seconds}s"
 
-@app.on_message(filters.command("suruse", HANDLER))
-async def restart_bot(client: Client, message: Message):
-    try:
-        await message.reply("Restarting the bot...")
-
-        # Send a termination signal to the app, causing it to restart
-        os.kill(os.getpid(), signal.SIGINT)
-
-    except Exception as e:
-        await message.reply(f"An error occurred while restarting: {str(e)}")
-        
 def run():
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
 
